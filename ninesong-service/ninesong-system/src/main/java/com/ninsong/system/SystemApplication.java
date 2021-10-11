5defea75bdf57e0e7e1462055d609243476962a7
wj <925283747@qq.com>
2021-10-11 21:48:43 +0800

    新增业务员服务

---
+++ ninesong-service/ninesong-system/src/main/java/com/ninsong/system/SystemApplication.java
@@ -0,0 +1,13 @@
+package com.ninsong.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SystemApplication.class,args);
+    }
+}
