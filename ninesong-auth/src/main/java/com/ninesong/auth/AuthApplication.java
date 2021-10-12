19c681f7b1de0946b36e8a92dea9589db7cc0e5c
jiawangjia <925283747@qq.com>
2021-10-12 08:50:05 +0800

    配置文件完善

--- ninesong-auth/src/main/java/com/ninesong/auth/AuthApplication.java
+++ ninesong-auth/src/main/java/com/ninesong/auth/AuthApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringCloudApplication
 @EnableDiscoveryClient
 public class AuthApplication {
     public static void main(String[] args) {
--- ninesong-service/ninesong-system/src/main/java/com/ninsong/system/SystemApplication.java
+++ ninesong-service/ninesong-system/src/main/java/com/ninsong/system/SystemApplication.java
@@ -2,9 +2,10 @@ package com.ninsong.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringCloudApplication
 @EnableDiscoveryClient
 public class SystemApplication {
     public static void main(String[] args) {
